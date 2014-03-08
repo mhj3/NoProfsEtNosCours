d8fb9815b962c163019105833cc80496c5f2d3aa
unknown <ppmhjeanty@gmail.com>
2014-03-08 00:36:48 -0500

    django

--- projetfinal_python/settings.py
+++ projetfinal_python/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -112,3 +112,4 @@ STATICFILES_DIRS = (
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
+
